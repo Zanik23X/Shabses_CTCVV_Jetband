3594ce39884dcc196f88b3e9a953d1e5f483f36a
Jony <zhanserik2003a@gmail.com>
2022-12-13 18:13:25 +0600

    this commit the 7 videos weeee

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -22,16 +22,26 @@
                     <thead>
                         <tr>
                             <th>ID</th>
+                            <th>Category</th>
                             <th>Post Name</th>
                             <th>Status</th>
+                            <th>Edit</th>
+
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($posts as $item)
                         <tr>
                             <td>{{$item->id}}</td>
+                            <td>{{$item->category->name}}</td>
                             <td>{{$item->name}}</td>
                             <td>{{$item->status == '1' ? 'Hidden':'Visible'}}</td>
+                            <td>
+                                <a href="{{ url('admin/post/' .$item->id) }}" class="btn btn-success">Edit</a>
+                            </td>
+                            <td>
+                                <a href="{{ url('admin/delete-post/' .$item->id) }}" class="btn btn-success">Delete</a>
+                            </td>
                         </tr>
                         @endforeach
                     </tbody>
