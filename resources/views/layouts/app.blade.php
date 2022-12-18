9dfe7867f63914b45ba4b86555844730fa7f434b
Jony <zhanserik2003a@gmail.com>
2022-12-18 15:55:21 +0600

    this commit larproj CTCV num-20 ou-e

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,19 +25,45 @@
     <!-- Styles -->
     <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/owl.theme.default.min.css') }}" rel="stylesheet">
+
+
 </head>
 <body>
     <div id="app">
         @include('layouts.inc.frontend-navbar')
 
-        <main class="py-4">
+        <main class="">
             @yield('content')
         </main>
     </div>
     <!-- Scripts -->
-    <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}" defer></script>
-    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}" defer></script>
-
+    <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}" ></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}" ></script>
+    <script src="{{ asset('assets/js/owl.carousel.min.js') }}" ></script>
     <script src="{{asset('js/app.js')}}"></script>
+    <script>
+        $('.category-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            dots:false,
+
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:4
+                }
+            }
+        })
+    </script>
+
+
 </body>
 </html>
