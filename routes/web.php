efce9070e2f1ba67581c42394d3859a1ff019cb5
Jony <zhanserik2003a@gmail.com>
2022-12-18 11:35:09 +0600

    laravel commit CTCV num-15

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::get('/',[App\Http\Controllers\Frontend\FrontendController::class,'index']);
 
+Route::get('tutorial/{category_slug}', [App\Http\Controllers\Frontend\FrontendController::class,'viewCategoryPost']);
+
 Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function (){
 
     Route::get('/dashboard',[App\Http\Controllers\Admin\DashboardController::class, 'index']);
